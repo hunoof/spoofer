586358f628f3b52785ad93a90c9f27576761ca7a
hunoof <65777121+hunoof@users.noreply.github.com>
2020-05-28 18:59:25 +0530

    Update spoofer.py

--- spoofer.py
+++ spoofer.py
@@ -1,10 +1,7 @@
 from clockwork import clockwork
 import time
- 
 api = clockwork.API("0a5b9f809d353f0404525a6a7a116909178991cb")
- 
 lyrics = open("lyrics.txt", "r").readlines()
- 
 for line in lyrics:
     payload = line.replace("\n", "")
     message = clockwork.SMS(from_name = "MICHHI PUTTU", to = "917754915707", message = payload)
